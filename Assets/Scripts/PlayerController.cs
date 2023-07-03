0a1c96dce7b3d08005b4c136c8d2a12327d59cbe
Ian <iancordoba4@gmail.com>
2023-07-02 23:31:44 -0300

    CorrecionDeMovimiento
    lo dejo basico. Cambie la perspectiva de la camara

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;          // Velocidad de movimiento del personaje
-    public float jumpForce = 5f;      // Fuerza de salto del personaje
+    public float speed = 5f;             // Velocidad de movimiento del personaje
 
     private Rigidbody rb;
 
@@ -16,46 +15,9 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // Obtener la entrada del jugador
+        // Movimiento horizontal
         float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        // Calcular el vector de movimiento
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
-        movement *= speed;
-
-        // Aplicar el movimiento al jugador
-        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
-
-        // Saltar si se presiona la tecla de salto (por ejemplo, "Espacio")
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
-    }
-
-    private void Jump()
-    {
-        // Comprobar si el personaje está en el suelo antes de saltar
-        if (IsGrounded())
-        {
-            // Aplicar una fuerza hacia arriba para realizar el salto
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
-    }
-
-    private bool IsGrounded()
-    {
-        // Raycast hacia abajo para comprobar si el personaje está en el suelo
-        float raycastDistance = 0.1f;
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance))
-        {
-            return true;
-        }
-
-        return false;
+        Vector3 moveDirection = new Vector3(moveHorizontal, 0f, 0f);
+        rb.velocity = new Vector3(moveDirection.x * speed, rb.velocity.y, 0f);
     }
 }
-
-
